2e4b38cdad6acaf773e2dc8a2dcd045e71b275b1
DJ Shitcoin™ <dj@shitcoin.jp>
2018-10-09 18:49:47 +0900

    add comments and indexes

--- db/migrate/20181009094010_create_withdrawals.rb
+++ db/migrate/20181009094010_create_withdrawals.rb
@@ -4,15 +4,19 @@ class CreateWithdrawals < ActiveRecord::Migration[5.2]
       t.string :symbol
       t.integer :user_id
       t.string :receiver_address
-      t.string :transaction_id
-      t.timestamp :submitted_at
-      t.decimal :amount
+      t.string :transaction_id, comment: 'This is the hash of the transaction'
+      t.timestamp :submitted_at, comment: 'The time when the transaction was submitted to the blockchain'
+      t.decimal :amount, precision: 50, scale: 20
       t.string :status
       t.integer :tries
       t.string :error
       t.timestamp :error_at
       t.string :signed_transaction
 
+      t.index :symbol
+      t.index :user_id
+      t.index :transaction_id
+      
       t.timestamps
     end
   end
