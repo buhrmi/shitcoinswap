93c1d674931e95d2462aed954dfb576ae0aee516
Yan Lin Aung <yanlinaung30@gmail.com>
2018-10-11 13:33:33 +0630

    Add password-reset function

--- config/routes.rb
+++ config/routes.rb
@@ -1,20 +1,22 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  
+
   namespace :admin do
     resources :balance_adjustments
   end
-  
+
   resources :withdrawals
 
-  
+
   resources :users
-  
+
+  resources :password_resets, only: [:new, :create, :edit, :update]
+
   post '/login'    => 'sessions#create'
   get '/login'     => 'sessions#new'
   delete '/logout' => 'sessions#destroy'
-  
+
   get 'balances', to: 'pages#balances'
 
-  root to: 'pages#welcome'  
+  root to: 'pages#welcome'
 end
