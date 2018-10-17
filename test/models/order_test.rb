22af25cce0302b9f2de86c1cb86b060fe446380b
DJ Shitcoin™ <dj@shitcoin.jp>
2018-10-17 15:32:18 +0900

    more order tests

--- test/models/order_test.rb
+++ test/models/order_test.rb
@@ -38,7 +38,7 @@ class OrderTest < ActiveSupport::TestCase
   test 'can submit sell-limit order with sufficient balance' do
     available_eth = alice.available_balance(eth)
     Order.create!(user: alice, rate: 1, kind: 'limit', side: 'sell', quantity: available_eth, base_asset: eth, quote_asset: jpy)
-    assert_equal 0, alice.available_balance(assets(:eth))
+    assert_equal 0, alice.available_balance(eth)
   end
 
   ## Order matching
@@ -58,6 +58,55 @@ class OrderTest < ActiveSupport::TestCase
     assert_equal(bob_jpy_before   + 15, bob.available_balance(jpy))
   end
 
+  ## Order matching
+  test "buy and sell limit orders fill each other partially" do
+    alice_jpy_before = alice.available_balance(jpy)
+    alice_eth_before = alice.available_balance(eth)
+    bob_jpy_before = bob.available_balance(jpy)
+    bob_eth_before = bob.available_balance(eth)
+
+    order1 = alice.orders.create!(side: 'buy', kind: 'limit', quantity: 7, rate: 6.5, base_asset: eth, quote_asset: jpy)
+    order2 = bob.orders.create!(side: 'sell', kind: 'limit', quantity: 3, rate: 5, base_asset: eth, quote_asset: jpy)
+    order2 = bob.orders.create!(side: 'sell', kind: 'limit', quantity: 3, rate: 6, base_asset: eth, quote_asset: jpy)
+    order2 = bob.orders.create!(side: 'sell', kind: 'limit', quantity: 3, rate: 7, base_asset: eth, quote_asset: jpy)
+    
+    assert_equal(alice_jpy_before - 7 * 6.5, alice.available_balance(jpy))
+    assert_equal(alice_eth_before,           alice.available_balance(eth))
+    assert_equal(bob_eth_before   - 9,       bob.available_balance(eth))
+    assert_equal(bob_jpy_before,             bob.available_balance(jpy))
+
+    order1.process!
+
+    assert_equal(alice_jpy_before - 7 * 6.5, alice.available_balance(jpy))
+    assert_equal(alice_eth_before + 6,       alice.available_balance(eth))
+    assert_equal(bob_eth_before   - 9,       bob.available_balance(eth))
+    assert_equal(bob_jpy_before   + 6 * 6.5, bob.available_balance(jpy))
+  end
+
+  test "buy and sell market orders fill each other partially" do
+    alice_jpy_before = alice.available_balance(jpy)
+    alice_eth_before = alice.available_balance(eth)
+    bob_jpy_before = bob.available_balance(jpy)
+    bob_eth_before = bob.available_balance(eth)
+
+    order1 = alice.orders.create!(side: 'sell', kind: 'limit', quantity: 7, rate: 2, base_asset: eth, quote_asset: jpy)
+    order2 = bob.orders.create!(side: 'buy', kind: 'market', total: 3, base_asset: eth, quote_asset: jpy)
+    order2 = bob.orders.create!(side: 'buy', kind: 'market', total: 3, base_asset: eth, quote_asset: jpy)
+    order2 = bob.orders.create!(side: 'buy', kind: 'market', total: 3, base_asset: eth, quote_asset: jpy)
+    order2 = bob.orders.create!(side: 'buy', kind: 'market', total: 6, base_asset: eth, quote_asset: jpy)
+    
+    assert_equal(alice_jpy_before,      alice.available_balance(jpy))
+    assert_equal(alice_eth_before -  7, alice.available_balance(eth))
+    assert_equal(bob_eth_before,        bob.available_balance(eth))
+    assert_equal(bob_jpy_before   - 15, bob.available_balance(jpy))
+
+    order1.process!
+
+    assert_equal(alice_jpy_before + 14, alice.available_balance(jpy))
+    assert_equal(alice_eth_before -  7, alice.available_balance(eth))
+    assert_equal(bob_eth_before   +  7, bob.available_balance(eth))
+    assert_equal(bob_jpy_before   - 15, bob.available_balance(jpy))
+  end
 
   def jpy
     assets(:jpy)
