182586771e0d4d8548ddd8ca10a11f2459bc16cb
Yan Lin Aung <yanlinaung30@gmail.com>
2018-10-18 15:56:13 +0630

    Add bootstrap design to login email sending form
    - change host for login in development

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -63,7 +63,7 @@ Rails.application.configure do
 
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  host = 'smtp.mailtrap.io'
+  host = 'localhost:3000'
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
     :address        => 'smtp.mailtrap.io',
