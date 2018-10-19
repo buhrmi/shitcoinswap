3b9774299d2455e24aedbbc1a05a71c4ba85d1b9
DJ Shitcoin™ <dj@shitcoin.jp>
2018-10-19 11:12:12 +0900

    add mailer config

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,4 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: 'from@example.com'
+  default from: 'NewArt Technology <support@newart.tech>'
   layout 'mailer'
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -93,4 +93,12 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.action_mailer.default_url_options = { host: ENV['DOMAIN'] }
+  ActionMailer::Base.smtp_settings = {
+    :address        => ENV['SMTP_SERVER'],
+    :user_name      => ENV['SMTP_USER'],
+    :password       => ENV['SMTP_PASSWORD'],
+    :port           => ENV['SMTP_PORT']
+  }
 end
