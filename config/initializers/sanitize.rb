e2008e3fff88ca8bff60a8f396465f5c7bfd64f8
DJ Shitcoin <dj@shitcoin.jp>
2018-10-28 02:45:50 +0900

    allow iframe attribs

--- config/initializers/sanitize.rb
+++ config/initializers/sanitize.rb
@@ -1,4 +1,8 @@
 module Sanitize::Config
   WHITELISTED = RELAXED.dup
   WHITELISTED[:elements] = WHITELISTED[:elements].dup << 'iframe'
+  WHITELISTED[:attributes] = WHITELISTED[:attributes].dup
+  WHITELISTED[:attributes]['iframe'] = ['width', 'height', 'src', 'frameborder', 'allow', 'allowfullscreen']
+  WHITELISTED[:protocols] = WHITELISTED[:protocols].dup
+  WHITELISTED[:protocols]['iframe'] = ['https']
 end
