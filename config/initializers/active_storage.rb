2a1482a04ea91b30d77069087cff9ce131988d99
DJ Shitcoin <dj@shitcoin.jp>
2018-10-28 00:36:18 +0900

    send blobs and variants without redirection/expiration

---
+++ config/initializers/active_storage.rb
@@ -0,0 +1,21 @@
+module ActiveStorage
+  class BlobsController  < BaseController
+    include ActiveStorage::SetBlob
+
+    def show
+      expires_in 1.year, public: true
+      send_data @blob.service.download(@blob.key),
+              type: @blob.content_type || DEFAULT_SEND_FILE_TYPE,
+              disposition: params[:disposition]
+    end
+
+    def show
+      expires_in 1.year, public: true
+      variant = @blob.representation(params[:variation_key]).processed
+      send_data @blob.service.download(variant.key),
+                type: @blob.content_type || DEFAULT_SEND_FILE_TYPE,
+                disposition: 'inline'
+    end
+    
+  end
+end
