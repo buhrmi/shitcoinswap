ccc533477a6c4d59c9d30f606b3be57adcf6bbe5
DJ Shitcoin™ <dj@shitcoin.jp>
2018-11-14 16:54:42 +0900

    fix order channel subscription and tagline

--- app/channels/orders_channel.rb
+++ app/channels/orders_channel.rb
@@ -1,7 +1,7 @@
 class OrdersChannel < ApplicationCable::Channel
   def subscribed
     stream_from 'orders', coder: ActiveSupport::JSON do |message|
-      if params[:asset_ids].try(:include?, message['base_asset_id'])
+      if params[:base_asset_ids].try(:include?, message['base_asset_id'])
         message.delete('user_id')
         transmit(message)
       elsif message['user_id'] == current_user.id
